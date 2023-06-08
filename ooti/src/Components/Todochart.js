8147b9cfcaac966d2d80875f56b3f4f01d7a51db
Florent Flo <adressemail22000@gmail.com>
2023-06-08 10:22:34 +0200

    todochart add percent

--- ooti/src/Components/Todochart.js
+++ ooti/src/Components/Todochart.js
@@ -1,14 +1,18 @@
 import "./Todochart.css";
 
 export default function Todochart(props) {
+
+  const checkedPercent = `${props.checked/props.length*100}%`;
+  const uncheckedPercent = `${props.unchecked/props.length*100}%`;
+
   return (
     <div className="todochart">
         <h2 className="todochart-title">Nombre de tâches de la Todo List : {props.length}</h2>
-        <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-            <div className="progress-bar bg-success" style={{width: "25%"}}>réalisées ({props.checked})</div>
+        <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow={checkedPercent} aria-valuemin="0" aria-valuemax="100">
+            <div className="progress-bar bg-success" style={{width: checkedPercent}}>réalisées ({props.checked})</div>
         </div>
-        <div className="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-            <div className="progress-bar bg-warning text-dark" style={{width: "75%"}}>à faire ({props.unchecked})</div>
+        <div className="progress" role="progressbar" aria-label="Warning example" aria-valuenow={uncheckedPercent} aria-valuemin="0" aria-valuemax="100">
+            <div className="progress-bar bg-warning text-dark" style={{width: uncheckedPercent}}>à faire ({props.unchecked})</div>
         </div>
     </div>
   )
